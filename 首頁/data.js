5a28ebec0c617709db4f7db10e924b3aaff444c9
wen9202 <160296016+wen9202@users.noreply.github.com>
2024-05-02 10:46:45 +0800

    c

--- 首頁/data.js
+++ 首頁/data.js
@@ -29,7 +29,13 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+	  {
+          "yaw": 1.6942073232673565,
+          "pitch": -0.027940042641924734,
+          "title": "松山高中",
+          "text": "<span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">前身台北市立</span><span style=\"color: rgb(4, 12, 40); background-color: rgb(211, 227, 253); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">松山</span><span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">初級中學，成立於民國四十七年八月；至五十七年八月，改制為台北市立</span><span style=\"color: rgb(4, 12, 40); background-color: rgb(211, 227, 253); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">松山</span><span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">國民中學． 民國七十六年七月，配合增設</span><span style=\"color: rgb(4, 12, 40); background-color: rgb(211, 227, 253); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">高中</span><span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">政策，成立</span><span style=\"color: rgb(4, 12, 40); background-color: rgb(211, 227, 253); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">松山高中</span><span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">籌備處，同年十月一日，由</span><span style=\"color: rgb(4, 12, 40); background-color: rgb(211, 227, 253); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">松山</span><span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">家商校長劉玉春先生兼任籌備處主任，民國七十七年七月，改由葉文堂先生任籌備處主任兼</span><span style=\"color: rgb(4, 12, 40); background-color: rgb(211, 227, 253); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px;\">松山</span><span style=\"color: rgb(31, 31, 31); font-family: &quot;Google Sans&quot;, arial, sans-serif; font-size: 20px; background-color: rgb(255, 255, 255);\">國中校長．</span>"
+        }]
     }
   ],
   "name": "首頁",
